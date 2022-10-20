bf2158c2df95d84bf06bafb9b5f6e942ca2460a7
Matheus Rocha <matheus.srocha@yahoo.com.br>
2022-10-19 23:14:35 -0300

    Inclusão de novos módulos

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,36 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CharacterModule } from './character/character.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import databaseconfig from './config/databaseconfig';
+import developconfig from './config/developconfig';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get<string>('database.host'),
+        port: configService.get<number>('database.port'),
+        username: configService.get<string>('database.username'),
+        password: configService.get<string>('database.password'),
+        database: configService.get<string>('database.database'),
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        synchronize: true,
+        autoLoadEntities: true,
+      })
+    }),
+    CharacterModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      cache: true,
+      load: [databaseconfig, developconfig]
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
